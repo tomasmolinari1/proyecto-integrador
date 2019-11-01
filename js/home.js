017a698cd8edd03d308b081e13fbe8e025fdd51c
Tomas Molinari <tmolinari@udesa.edu.ar>
2019-11-01 14:34:39 -0300

    Java series populares y mejor puntaje

---
+++ js/home.js
@@ -0,0 +1,47 @@
+window.onload= function () {
+  var apiKey ="81abb78b34be12fc4620b0a001276f5a"
+
+  fetch ("https://api.themoviedb.org/3/tv/popular?api_key=81abb78b34be12fc4620b0a001276f5a&language=en-US&page=1")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(dataTMBD) {
+    console.log(dataTMBD);
+    var popularContainer = document.querySelector(".populars")
+    var titulo = ""
+    var img_src = ""
+    var serie = ""
+    var urlFija = "sacar de la consigna"
+    for (var i = 0; i < dataTMBD.results.length; i++) {
+      console.log (dataTMBD.results[i].name);
+
+      titulo = dataTMBD.results[i].name
+      img_src = urlFija + dataTMBD.results[i].poster_path
+
+      serie =   '<article class="">'
+      serie +=      '<h2>'+ titulo +'</h2>'
+      serie +=      '<img src="'+ img_src +'" alt="">'
+      serie +=  '</article>'
+
+      popularContainer.innerHTML += serie
+    }
+  })
+
+
+  fetch ("https://api.themoviedb.org/3/tv/top_rated?api_key=81abb78b34be12fc4620b0a001276f5a&language=en-US&page=1")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(dataTMBD) {
+
+    var popularContainer = document.querySelector(".mejor-puntaje")
+    for (var i = 0; i < dataTMBD.results.length; i++) {
+      console.log (dataTMBD.results[i].name);
+
+      popularContainer.innerHTML
+    }
+
+  })
+
+
+}
