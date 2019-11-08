9ac6549749469a7655a4e4ab2778a8fc1f079367
Tomas Molinari <tmolinari@udesa.edu.ar>
2019-11-08 12:16:21 -0300

    Pagina 3 sin estilo

--- js/seriesPorGenero.js
+++ js/seriesPorGenero.js
@@ -1,42 +1,42 @@
 
 window.addEventListener("load", function(){
 
+  var urlParams = new URLSearchParams(location.search);
+
+  var idDeGenero = urlParams.get("idDeGenero");
+
+  console.log(idDeGenero);
+
   var apiKey ="81abb78b34be12fc4620b0a001276f5a";
   var urlFija = "https://image.tmdb.org/t/p/original/";
   var urlGenero = "seriesPorGenero.html?idDeGenero=";
 
-  function queryTMDB() {
-    return fetch ("https://api.themoviedb.org/3/discover/tv?api_key=" " + apiKey +" &sort_by=popularity.desc&page=1&with_genres="
-")
-      .then(function(response) {
-        return response.json();
-      })
-  }
-  function createItemHtml(clase, titulo, id) {
-    var genre = '<li class="'+ clase + '">'
-    genre +=      '<h2><a href="'+ urlGenero + id +'" >'+ titulo +'</a></h2>'
-    genre +=  '</li>';
+  var urlAPI = "https://api.themoviedb.org/3/discover/tv?api_key=" + apiKey +"&sort_by=popularity.desc&page=1&with_genres=" + idDeGenero
 
-    return genres;
+  function queryTMDB() {
+    return fetch(urlAPI).then(function(response) { return response.json() })
+  }
+
+  function createItemHtml(clase, data) {
+    var content = '<li class="'+ clase + '">'
+    // genre +=      '<h2><a href="'+ urlGenero + id +'" >'+ titulo +'</a></h2>'
+    // genre +=  '</li>';
+    content += '<h2>' + data.name + '</h2>';
+    content += '<img src="' + (data.poster_path ? urlFija + data.poster_path : 'images/cine.jpg') + '" width="100">';
+    content += '</li>';
+
+    return content;
   }
 
   queryTMDB()
   .then(function(dataTMBD) {
     console.log(dataTMBD);
-    var genresContainer = document.querySelector(".series-por-genero")
-    var titulo = ""
-      console.log(titulo);
+    var body = document.querySelector("body")
 
-
-    for (var i = 0; i < dataTMBD.genres.length; i++) {
-
-
-      titulo = dataTMBD.genres[i].name
-
-
-      seriesPorGeneroContainer.innerHTML += createItemHtml("series-por-genero-item", titulo, dataTMBD.genres[i].id);
+    for (var i = 0; i < dataTMBD.results.length; i++) {
+      // titulo = dataTMBD.genres[i].name
+      body.innerHTML += createItemHtml("series-por-genero-item", dataTMBD.results[i]);
     }
-    console.log(titulo);
   })
 
 })
